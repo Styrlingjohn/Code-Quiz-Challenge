cef8fe960087fcd0af2fa0a15026e69d968d3583
Styrlingjohn <styrling_morris@yahoo.com>
2022-11-15 21:25:11 -0700

    added JavaScript to form a button and appended the countdown to it to start the timer.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
 // First javascript add
+var timerEl = document.getElementById('timer');
+
+function countdown() {
+    var timeLeft = 60;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '0';
+        // Use `clearInterval()` to stop the timer
+        timerEl.textContent = 'Times Up';
+      }
+    }, 1000);
+    getStarted.appendChild('countdown');
+}
